c3ac1fe7761c0e339686379c1338f24deb3d2580
harryxu <harryzhxu@gmail.com>
2014-08-29 02:46:25 +0800

    init

---
+++ data/puppet/site.pp
@@ -0,0 +1,15 @@
+#file { '/etc/apt/sources.list':
+  #mode => 644,
+  #owner => root,
+  #group => root,
+  #source => 'puppet:///files/apt/sources.list',
+#}
+
+include 'docker'
+
+class { 'ohmyzsh': }
+ohmyzsh::install { ['vagrant', 'root']: }
+ohmyzsh::plugins { 'vagrant': plugins => 'git docker ' }
+ohmyzsh::theme { ['vagrant']: theme => 'robbyrussell' }
+
+
