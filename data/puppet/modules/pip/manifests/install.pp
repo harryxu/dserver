07ebdc9e9f620f2cf14c2e54f528f5296bfab876
harryxu <harryzhxu@gmail.com>
2014-08-29 18:02:29 +0800

    Add pip puppet module and install fig.

---
+++ data/puppet/modules/pip/manifests/install.pp
@@ -0,0 +1,41 @@
+
+define pip::install(
+  $package = $title,
+  $version = undef,
+  $python_version = '2.7',
+  $ensure = present,
+) {
+
+  Exec {
+    path => ['/usr/local/bin', '/usr/bin', '/bin'],
+  }
+
+  case $ensure {
+    present, installed: {
+      if $version != undef {
+        $package_with_version = "$package==$version"
+        $grep_for = "^$package_with_version$"
+      } else {
+        $package_with_version = $package
+        $grep_for = "^$package=="
+      }
+
+      exec { "install-$package":
+        command => "pip$python_version install $package_with_version",
+        unless  => "pip$python_version freeze | grep '$grep_for'",
+      }
+    }
+
+    latest: {
+      # TODO Read from https://pypi.python.org/pypi/<package_name>/json
+      # Probably best if implemented in Ruby
+    }
+
+    absent, purged: {
+      exec { "uninstall-$package":
+        command => "pip$python_version uninstall $package -y",
+        onlyif  => "pip$python_version freeze | cut -d= -f1 | egrep '^$package$'",
+      }
+    }
+  }
+}
--- data/puppet/site.pp
+++ data/puppet/site.pp
@@ -2,6 +2,11 @@ include git
 
 include 'docker'
 
+include pip
+pip::install { 'fig':
+ ensure => present,
+}
+
 class { 'ohmyzsh': }
 ohmyzsh::install { ['vagrant', 'root']: }
 ohmyzsh::plugins { 'vagrant': plugins => 'git docker ' }
