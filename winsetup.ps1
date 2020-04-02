d3554a5a8a04486e41871104a4c97593c47cb4c2
harryxu <harryzhxu@gmail.com>
2020-04-02 17:04:10 +0800

    Add windows software setup script.

---
+++ winsetup.ps1
@@ -0,0 +1,8 @@
+# Set-ExecutionPolicy AllSigned
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install virtualbox
+choco install vagrant
+# vagrant plugin install vagrant-vbguest
+# vagrant plugin install vagrant-env
